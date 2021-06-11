a1f6e407ff24934ec5b374ca6964efa2b8c691b8
Brittany Doss <brinidoss@gmail.com>
2021-06-11 17:23:33 -0400

    all tests working, constructors and 3/3 methods

--- src/js/ChangeHandler.js
+++ src/js/ChangeHandler.js
@@ -32,24 +32,73 @@ class ChangeHandler {
      */
     isPaymentSufficient() {
       // TODO
+
+      if (this.cashTendered < this.amountDue) {
+        return false;
+      } else if (this.cashTendered > this.amountDue) {
+        return true;
+      } else if (this.cashTendered === this.amountDue) {
+        return true;
+      } else {
+        return false;
+      }
     }
 
     giveChange() {
         // TODO return the correct change in the following format...
         //if else down below thanks to Kyle
-        if (cashTendered >= 25) {
-          this.cashTendered -= 25;
-          obj.quarters++;
-        } else if (this.cashTendered >= 10) {
 
+        let coins = {
+          quarters: 0,
+          dimes: 0,
+          nickels: 0,
+          pennies: 0
         }
-        return {
-            quarters: 0,
-            dimes: 0,
-            nickels: 0,
-            pennies: 0
+        if (this.cashTendered > this.amountDue) {
+          //console.log(changeHandler32);
+          this.cashTendered -= this.amountDue;
+          this.amountDue -= this.amountDue;
+        
+        while (this.cashTendered > this.amountDue) {
+          
+          if (this.cashTendered >= 25) {
+            //we have enough cashTendered to add at least one more quarter
+            coins.quarters++;
+            this.cashTendered -= 25;
+          } else if (this.cashTendered >= 10) {
+            coins.dimes++;
+            this.cashTendered -= 10;
+          } else if (this.cashTendered >= 5) {
+            coins.nickels++;
+            this.cashTendered -= 5;
+          } else if (this.cashTendered >= 1) {
+            coins.pennies++;
+            this.cashTendered -= 1;
+          }
         }
+      }
+        return coins;
+        
+        // {
+        //     quarters: 0,
+        //     dimes: 0,
+        //     nickels: 0,
+        //     pennies: 0
+        // }
     }
 }
+
+// //breaking down 32 change
+// const changeHandler32 = new ChangeHandler(10);
+// console.log(changeHandler32);
+// changeHandler32.insertCoin('quarter');
+// changeHandler32.insertCoin('dime');
+// changeHandler32.insertCoin('nickel');
+// changeHandler32.insertCoin('penny');
+// changeHandler32.insertCoin('penny');
+// console.log(changeHandler32);
+// console.log(changeHandler32.giveChange());
+// console.log(changeHandler32);
+
 
 module.exports = ChangeHandler;
--- tests/ChangeHandler.test.js
+++ tests/ChangeHandler.test.js
@@ -6,7 +6,47 @@ const changeHandlerNickel = new ChangeHandler();  //for the nickel
 const changeHandlerPenny = new ChangeHandler();  //for the penny
 const changeHandlerCallAgain = new ChangeHandler();
 const changeHandlerDifferentCoins = new ChangeHandler();
+const changeHandlerLessThan = new ChangeHandler(50);
+const changeHandlerMoreThan = new ChangeHandler(50);
+const changeHandlderEqualTo = new ChangeHandler(50);
+const changeHandler32 = new ChangeHandler(10);
+const changeHandler10 = new ChangeHandler(10);
+const changeHandler27 = new ChangeHandler(10);
+const changeHandler68 = new ChangeHandler(10);
+const changeHandler0 = new ChangeHandler(10);
 
+const change32 = {
+  quarters: 1,
+  dimes: 0,
+  nickels: 1,
+  pennies: 2
+}
+const change10 = {
+  quarters: 0,
+  dimes: 1,
+  nickels: 0,
+  pennies: 0
+}
+const change27 = {
+  quarters: 1,
+  dimes: 0,
+  nickels: 0,
+  pennies: 2
+}
+
+const change68 = {
+  quarters: 2,
+  dimes: 1,
+  nickels: 1,
+  pennies: 3
+}
+
+const change0 = {
+  quarters: 0,
+  dimes: 0,
+  nickels: 0,
+  pennies: 0
+}
 
 changeHandlerQuarter.insertCoin('quarter');
 changeHandlerDime.insertCoin('dime');
@@ -23,7 +63,35 @@ changeHandlerDifferentCoins.insertCoin('dime');
 changeHandlerDifferentCoins.insertCoin('nickel');
 changeHandlerDifferentCoins.insertCoin('penny');
 
+changeHandlerLessThan.insertCoin('dime');
+changeHandlerMoreThan.insertCoin('quarter');
+changeHandlerMoreThan.insertCoin('quarter');
+changeHandlerMoreThan.insertCoin('dime');
+changeHandlderEqualTo.insertCoin('quarter');
+changeHandlderEqualTo.insertCoin('quarter');
 
+changeHandler32.insertCoin('quarter');
+changeHandler32.insertCoin('dime');
+changeHandler32.insertCoin('nickel');
+changeHandler32.insertCoin('penny');
+changeHandler32.insertCoin('penny');
+
+changeHandler10.insertCoin('dime');
+changeHandler10.insertCoin('dime');
+
+changeHandler27.insertCoin('quarter');
+changeHandler27.insertCoin('dime');
+changeHandler27.insertCoin('penny');
+changeHandler27.insertCoin('penny');
+
+changeHandler68.insertCoin('quarter');
+changeHandler68.insertCoin('quarter');
+changeHandler68.insertCoin('quarter');
+changeHandler68.insertCoin('penny');
+changeHandler68.insertCoin('penny');
+changeHandler68.insertCoin('penny');
+
+changeHandler0.insertCoin('dime');
 
 describe("ChangeHandler", function() {
   test("class is defined", function() {
@@ -64,9 +132,41 @@ describe("ChangeHandler", function() {
 
   test('calling function continues to add - different coins', () => {
     expect(changeHandlerDifferentCoins.cashTendered).toEqual(41);
-  })
-  // TODO: Add additional tests below...
+  });
 
+  test('iPS returns fale if cT less than aD', () => {
+    expect(changeHandlerLessThan.isPaymentSufficient()).toEqual(false);
+  });
+
+  test('iPS returns true if cT more than aD', () => {
+    expect(changeHandlerMoreThan.isPaymentSufficient()).toEqual(true);
+  });
+
+  test('iPS returns true if cT equal to aD', () => {
+    expect(changeHandlderEqualTo.isPaymentSufficient()).toEqual(true);
+  });
+
+  //giveChange tests
+
+  test('32 change', () => {
+    expect(changeHandler32.giveChange()).toEqual(change32);
+  });
+
+  test('10 change', () => {
+    expect(changeHandler10.giveChange()).toEqual(change10);
+  });
+
+  test('27 change', () => {
+    expect(changeHandler27.giveChange()).toEqual(change27);
+  });
+
+  test('68 change', () => {
+    expect(changeHandler68.giveChange()).toEqual(change68);
+  });
+
+  test('0 change', () => {
+    expect(changeHandler0.giveChange()).toEqual(change0);
+  });
 
 });
 
@@ -74,11 +174,9 @@ describe("ChangeHandler", function() {
 Required Test Cases 
 
 
-isPaymentSufficient:
-  Returns true if cashTendered more than amountDue.
-  Returns false if cashTendered less than amountDue.
-  Returns true if cashTendered equal to amountDue.
+
 giveChange:
+test all types of coins and 0
   32 change produces: quarters: 1, dimes: 0, nickels: 1, pennies: 2.
   10 change produces: quarters: 0, dimes: 1, nickels: 0, pennies: 0.
   27 change produces: quarters: 1, dimes: 0, nickels: 0, pennies: 2.
